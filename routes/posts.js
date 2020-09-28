809f2b4cabb6e1bf7a676d3b149bbdeca024d6ae
Himanshuranjan30 <himanshuranjan30@gmail.com>
2020-09-28 13:37:38 +0530

    crud added

--- routes/posts.js
+++ routes/posts.js
@@ -1,26 +1,37 @@
-const express= require('express');
-const router= express.Router();
-const userpost= require('../model/post');
+const express = require('express');
+const router = express.Router();
+const userpost = require('../model/post');
 
-router.get('/', (req, res) => {
-    res.send('Hey Thats a post')
+router.get('/', async (req, res) => {
+    const posts = await userpost.find();
+    res.json(posts);
 })
 
-router.get('/specific',(req,res)=>{
-    res.send('specific post')
+router.get('/:postid', async (req, res) => {
+    const post = await userpost.findById(req.params.postid)
+    res.json(post)
 })
 
-router.post('/',(req,res)=>{
-    console.log(req.body.title)
-    
-    const Post= new userpost({
+router.post('/', async (req, res) => {
+
+
+    const Post = new userpost({
         title: req.body.title,
         description: req.body.description,
     })
-    
-    Post.save().then(data=>{
-        res.json(data);
-    })
+    const Savedpost = await Post.save();
+    res.json(Savedpost);
+
 })
 
-module.exports=router;
+router.delete('/:postid',async(req,res)=>{
+    const deletedpost= await userpost.remove({_id: req.params.postid});
+    res.json(deletedpost)
+})
+
+router.patch('/:postid',async(req,res)=>{
+    const updatedpost= await userpost.updateOne({_id:req.params.postid},{$set:{title:req.body.title}})
+    res.json(updatedpost)
+})
+
+module.exports = router;
